4d7a0ffc50a1b520e1246a60b63a5999541c019c
ronsinda <ronsinda@gmail.com>
2014-04-12 15:12:51 -0700

    added a test class

---
+++ Git Test/ABTest.h
@@ -0,0 +1,13 @@
+//
+//  ABTest.h
+//  Git Test
+//
+//  Created by ronbehee on 4/12/14.
+//  Copyright (c) 2014 almega brainware LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABTest : NSObject
+
+@end
---
+++ Git Test/ABTest.m
@@ -0,0 +1,13 @@
+//
+//  ABTest.m
+//  Git Test
+//
+//  Created by ronbehee on 4/12/14.
+//  Copyright (c) 2014 almega brainware LLC. All rights reserved.
+//
+
+#import "ABTest.h"
+
+@implementation ABTest
+
+@end
